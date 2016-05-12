a6bf7b53a6d4b72f2814a33cc4d10dff04ae9e71
sophiakaz <sophiakaz98@gmail.com>
2016-05-12 11:58:58 -0700

    Update Receive.java

--- RC5/src/RC5/Receive.java
+++ RC5/src/RC5/Receive.java
@@ -4,17 +4,24 @@ public class Receive implements InterfaceE {
 
 
 	private int[] s; // The expanded list of words derived from the key, of length 2(r+1), with each element being a word
+	private int t; // length of s
 	private int[] l; // A convenience to encapsulate K as an array of word-sized values rather than byte-sized.
 	private int b; // The length of the key in bytes.
 	private int u; // The length of a word in bytes. 
+	private int c; // 
 	private int w; // The length of a word in bits. Typical values of this in RC5 are 16, 32, and 64. Note that a "block" is two words long.
 	private byte[] key; // The key, considered as an array of bytes (using 0-based indexing).
 	private int rounds; // The number of rounds to use when encrypting data.
+	
 
 	public Receive(String a, int round){
 		String str = a;
 		key = getKeyFromString(str);
-		
+		rounds = round;
+		b=key.length();
+		t = (int)(2 * (rounds + 1));
+		c = Math.max(b, 1) / u;
+
 	}
 	
 	public String encrypt(String a){
