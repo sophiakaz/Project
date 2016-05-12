59b098e23e25d73e94c25656f65a5ec05024872f
tranj8229 <jujutran99@gmail.com>
2016-05-12 12:07:16 -0700

    Update Transmit.java

--- RC5/src/RC5/Transmit.java
+++ RC5/src/RC5/Transmit.java
@@ -11,4 +11,14 @@ public class Transmit implements InterfaceE {
 		
 	}
 	
+	private byte[] GetStringFromKey(byte[] keybites){
+		char[] keychar = new char[keybites.length];
+		for (int i = 0; i < keybites.length; i++){
+			keychar[i] = (char)keybites[i];
+		}
+		String str= "";
+		for (int j = 0; j<keychar.length; j++)
+		{
+			str += keychar[i];
+		}
 }
