ddc5d7f37fff10920d8735a8d3386d023526d7a2
sophiakaz <sophiakaz98@gmail.com>
2016-05-13 12:47:34 -0700

    Update RC5-Runner.java

--- RC5/src/RC5/RC5-Runner.java
+++ RC5/src/RC5/RC5-Runner.java
@@ -1,9 +1,26 @@
-public class RC5-Runner
-{ 
-	public static void main(String [] args)
-	{
-	  Receive test = new Receive("testing testing", 5);
-	  
+package RC5;
+
+import java.util.Arrays;
+
+public class RC5-Runner {
+	public static void main(String[] args) {
+		//Receive test = new Receive("testing testing", 5);
+
+		byte[] bytetest = new byte[] { (byte) 0xe0, (byte) 0x4f, (byte) 0xd0,
+				(byte) 0x20, (byte) 0xea };
+		
+		//System.out.println(Arrays.toString(bytetest));
+		System.out.println((int)(bytetest[0] + bytetest[1]) + " " + (int)(bytetest[2] + bytetest[3]));
+
+		Transmit demo = new Transmit(bytetest, 2);
+		System.out.println(demo.encode((int)(bytetest[0] + bytetest[1]),(int)(bytetest[2] + bytetest[3])));
+		
+		Receive demo2 = new Receive(bytetest, 2);
+		System.out.println(demo2.decrypt(-15464, -1620917327));
+		
+		System.out.println(demo.encode(-514935701, 514935702));
+		
+
 	}
 
 }
