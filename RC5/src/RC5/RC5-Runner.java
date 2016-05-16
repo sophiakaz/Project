208e9c3b7c6539b96165b31266efac9ae32398be
tranj8229 <jujutran99@gmail.com>
2016-05-16 00:21:18 -0700

    Update RC5-Runner.java

--- RC5/src/RC5/RC5-Runner.java
+++ RC5/src/RC5/RC5-Runner.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class RC5Runner {
 	public static void main(String[] args) {
+		
 		Transmit test = new Transmit();
 		System.out.println(test.generateKey("pass"));
 		System.out.println(test.encrypt("encrypt this"));
@@ -11,9 +12,7 @@ public class RC5Runner {
 		Receive demo = new Receive();
 		System.out.println(demo.generateKey("pass"));
 		System.out.println(demo.decrypt("ëë55^^BBxxãã"));
-
 		
-
 	}
 
 }
