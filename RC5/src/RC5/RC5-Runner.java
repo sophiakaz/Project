88d34b360ab73ca1833f991ea8bd74ea9fe50e82
tranj8229 <jujutran99@gmail.com>
2016-05-16 00:21:06 -0700

    Update RC5-Runner.java

--- RC5/src/RC5/RC5-Runner.java
+++ RC5/src/RC5/RC5-Runner.java
@@ -4,19 +4,13 @@ import java.util.Arrays;
 
 public class RC5Runner {
 	public static void main(String[] args) {
-		//Receive test = new Receive("testing testing", 5);
-
-		byte[] bytetest = new byte[] { (byte) 0xe0, (byte) 0x4f, (byte) 0xd0,
-				(byte) 0x20, (byte) 0xea };
+		Transmit test = new Transmit();
+		System.out.println(test.generateKey("pass"));
+		System.out.println(test.encrypt("encrypt this"));
 		
-		//System.out.println(Arrays.toString(bytetest));
-		//System.out.println((int)(bytetest[0] + bytetest[1]) + " " + (int)(bytetest[2] + bytetest[3]));
-
-		Transmit demo = new Transmit(bytetest, 2);
-		System.out.println(demo.encrypt(25234, 30457));
-		
-		Receive demo2 = new Receive(bytetest, 2);
-		System.out.println(demo2.decrypt(-3484, 1172299431));
+		Receive demo = new Receive();
+		System.out.println(demo.generateKey("pass"));
+		System.out.println(demo.decrypt("ëë55^^BBxxãã"));
 
 		
 
