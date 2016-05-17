879f4246cd4fc31b60a8bcbb9d059d8575f1e0b7
tranj8229 <jujutran99@gmail.com>
2016-05-17 12:51:11 -0700

    Update Interface.java

--- RC5/src/RC5/Interface.java
+++ RC5/src/RC5/Interface.java
@@ -1,4 +1,7 @@
-public interface Interface{
-  
+package RC5;
 
+public interface Interface{
+  public String encrypt(String plainText);
+  public String decrypt(String cipherText);
+  public String generateKey(String password);
 }
