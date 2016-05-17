5d9e638ed73cc9de8cc49bd544bf88e4d9c44226
tranj8229 <jujutran99@gmail.com>
2016-05-17 10:38:30 -0700

    Update RC5-Runner.java

--- RC5/src/RC5/RC5-Runner.java
+++ RC5/src/RC5/RC5-Runner.java
@@ -5,14 +5,39 @@ import java.util.Arrays;
 public class RC5Runner {
 	public static void main(String[] args) {
 		
-		Transmit test = new Transmit();
-		System.out.println(test.generateKey("pass"));
-		System.out.println(test.encrypt("encrypt this"));
-		
-		Receive demo = new Receive();
-		System.out.println(demo.generateKey("pass"));
-		System.out.println(demo.decrypt("ëë55^^BBxxãã"));
-		
-	}
+		int i;
+	       int j;
+	       int b = 16;
+	       int[] pt1 = new int[2];
+	       int[] pt2 = new int[2];
+	       int[] ct = {1,2};
+	       byte[] key = new byte[b];
+	       
+	        
+	       System.out.print("RC5-32/12/16 examples:\n");
+	       for (i = 1;i < 6;i++){ // Initialize pt1 and key pseudo-randomly based on previous ct
+	         pt1[0] = ct[0];
+	         pt1[1] = ct[1];
 
+	         for (j = 0;j < b;j++){
+	             key[j] = (byte) (ct[0] % (255 - j));
+	         }
+	         /* Setup, encrypt, and decrypt */
+	         
+	         Transmit h = new Transmit(key, pt1, ct);
+		     Receive r = new Receive(key, ct, pt2);
+
+	         /* Print out results, checking for decryption failure */
+	         System.out.printf("\n%d. key = ",i);
+	         for (j = 0; j < b; j++){
+	             System.out.print((key[j])+" ");
+	         }
+	         System.out.println("\n   Inputdata:"  + pt1[0]  + pt1[1] +   " ---> " +  "Encrypted:" + ct[0]+ct[1]);
+	         System.out.println("   Decrypted:"  + pt2[0]  + pt2[1] );
+
+	         if (pt1[0] != pt2[0] || pt1[1] != pt2[1]){
+	             System.out.print("Decryption Error!");
+	         }
+	       }
+	  }
 }
