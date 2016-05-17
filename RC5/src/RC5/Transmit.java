ff9e08f96aacd5c25f5602adaf4cc23762397c93
sophiakaz <sophiakaz98@gmail.com>
2016-05-17 12:12:15 -0700

    Update Transmit.java

--- RC5/src/RC5/Transmit.java
+++ RC5/src/RC5/Transmit.java
@@ -1,6 +1,6 @@
 package RC5;
 
-public class Transmit  {
+public class Transmit implements InterfaceE {
 
 
 	private int[] s; // The expanded list of words derived from the key, of length 2(r+1), with each element being a word
