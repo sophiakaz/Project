6c5902158dfcdb9c765e4c7f35a5768b8392ceb7
tranj8229 <jujutran99@gmail.com>
2016-05-18 12:00:34 -0700

    Update Transmit.java

--- RC5/src/RC5/Transmit.java
+++ RC5/src/RC5/Transmit.java
@@ -1,137 +1,65 @@
 package RC5;
 import java.util.Arrays;
 
-public class Transmit  {
-
-
-	private int[] s; // The expanded list of words derived from the key, of length 2(r+1), with each element being a word
-	private int t; // integer 2 * (rounds + 1)
-	private int[] L; // A convenience to encapsulate K as an array of word-sized values rather than byte-sized.
-	private int b; // The length of the key in bytes.
-	private int u; // The length of a word in bytes. 
-	private int c; // Math.max(b, 1) / u;
-	private int w; // The length of a word in bits (16). Typical values of this in RC5 are 16, 32, and 64. Note that a "block" is two words long(32).
-	private int rounds; // The number of rounds to use when encrypting data.
-
-	
-	public Transmit(){
-		rounds=12;
-		w=16;
-		u=w/8;
-		//c = Math.max(b, 1) / u;
-		t = (int)(2 * (rounds + 1));
-		s = new int[t];
-	}
-	
-	public String encrypt(String plainText){
-		byte[] ptbytes = plainText.getBytes();
-		int[] pt = new int[(ptbytes.length)/2];   //pt is plaintext
-		int[] ct = new int[pt.length];			//ct is ciphertext
-		for (int a = ptbytes.length-1; a!=-1; a--)
-		{
-			pt[a/u] = leftRotate(pt[a/u],8) + ptbytes[a];
-		}
-		
-		for (int loop = 0; loop < pt.length - 1; loop=loop+2)
-		{
-			int A = pt[loop] + s[loop];
-			int B = pt[loop+1] + s[loop+1];
-			for(int i =1; i<=rounds; i++){
-				A = leftRotate(A ^ B, B) + s[2 * i];
-				B = leftRotate(B ^ A, A) + s[2 * i + 1];
-			}
-			ct[loop] = A;
-			ct[loop+1] = B;
-		}
-		
-		byte[] ctbytes = new byte[ptbytes.length];
-		for (int a = ctbytes.length-1; a!=-1; a--)
-		{
-			ctbytes[a] = (byte)(ct[a/u] - (ct[a/u]>>>8));	//REVERSED
-		}
-		String encrypted = new String(ctbytes);
-		return encrypted;
-	}
-
-	public String decrypt(String cipherText){
-		byte[] ctbytes = cipherText.getBytes();
-		int[] ct = new int[(ctbytes.length)/2];   //pt is plaintext
-		int[] pt = new int[ct.length];			//ct is ciphertext
-		
-		for (int a = ctbytes.length-1; a!=-1; a--)
-		{
-			ct[a/u] = leftRotate(ct[a/u],8) + ctbytes[a];
-		}
-		
-		for (int loop = ct.length - 1; loop > 0; loop=loop-2)
-		{
-		int A = 0;
-		int B = 0;
-		for (int i=rounds; i>=1; i--){
-				
-				B = (((B - s[2 * i + 1])>>> A)) ^ A;
-				A = (((A - s[2 *i]) >>> B)) ^ B;
-			}
-			pt[loop] = B - s[loop];
-			pt[loop-1] = A - s[loop-1];
-		}
-		
-		byte[] ptbytes = new byte[ctbytes.length];
-		for (int a = ptbytes.length-1; a!=-1; a--)
-		{
-			ptbytes[a] = (byte)(pt[a/u] - (pt[a/u]>>>8));	//REVERSED
-		}
-		String decrypted = new String(ptbytes);
-		return decrypted;
-				
-	}
-	
-	public String generateKey(String password){
-		int i, j;
-		int A, B;
-		byte[] key = password.getBytes();
-		b = key.length;
-		int Pw = 0xb7e1;
-		int Qw = 0x9e37; 
-		c = (int)Math.max(1,Math.ceil(8*b/w));
-		L = new int[c];
-		L[c-1] = 0;
-		for (int z = b-1; z!=-1; z--)
-		{
-			L[z/u] = (L[z/u]<<8) + key[z];
-		}
-		
-		
-		s[0] = Pw;
-		for (int a = 1; a <= t-1; a++)
-		{
-			s[a] = s[a-1] + Qw;
-		}
-		A=B=i=j=0;
-		int max = 3 * (Math.max(t, c));
-		for(int k = 0; k < max; k++)
-		{
-			A = s[i] = leftRotate((s[i] + A + B), 3);
-			B = L[j] = leftRotate((L[j] + A + B), (A + B));
-			i = (i + 1) % t;
-			j = (j + 1) % c;
-		}
-		return Arrays.toString(s);
-	}
-
-
-	private int leftRotate(int x, int offset) {
-		int t1, t2;
-		t1 = x >> (16 - offset);
-		t2 = x << offset;
-		return (t1 | t2);
-	}
-
-	private int rightRotate(int x, int offset) {
-		int t1, t2;
-		t1 = x << (16 - offset);
-		t2 = x >> offset;
-		return (t1 | t2);
-	}
-	
+public class Transmit implements Interface {
+	public static final int w = 32;
+    public static final int r = 12;
+    public static final int b = 16;
+    public static final int c = 4;
+    public static final int t = 26;
+     
+    public int[] S = new int[t]; // expanded key table
+    public int P = (int)0xb7e15163; // magic constants for w=32bits
+    public int Q = (int)0x9e3779b9;
+    
+    public Transmit(byte[] key, int[] pt, int[] ct){
+    	setupKey(key);
+    	encrypt(pt,ct);
+    }
+    
+    public void encrypt(int[] pt, int[] ct){ // input=pt/output=ct
+        int i;
+        int A = pt[0] + S[0];
+        int B = pt[1] + S[1];
+        for (i = 1; i <= r; i++){
+            A = (((A ^ B) << (B & (w - 1))) | ((A ^ B)>>>(w - (B & (w - 1))))) + S[2 * i];
+            B = (((B ^ A) << (A & (w - 1))) | ((B ^ A)>>>(w - (A & (w - 1))))) + S[2 * i + 1];
+        }
+        ct[0] = A;
+        ct[1] = B;
+    }
+    
+    public void decrypt(int[] ct, int[] pt){ // input=ct/output=pt
+        int i;
+        int B = ct[1];
+        int A = ct[0];
+        for (i = r; i > 0; i--){
+            B = (((B - S[2 * i + 1])>>>(A & (w - 1))) | ((B - S[2 * i + 1]) << (w - (A & (w - 1))))) ^ A;
+            A = (((A - S[2 * i])>>>(B & (w - 1))) | ((A - S[2 * i]) << (w - (B & (w - 1))))) ^ B;
+        }
+        pt[1] = B - S[1];
+        pt[0] = A - S[0];
+    }
+    
+    public void setupKey(byte[] K){ // secret input key K[0...b-1]
+        int i;
+        int j;
+        int k;
+        int u = w / 8;
+        int A;
+        int B;
+        int[] L = new int[c];
+        /* Initialize L, then S, then mix key into S */
+        for (i = b - 1,L[c - 1] = 0; i != -1; i--){
+            L[i / u] = (L[i / u] << 8) + K[i];
+        }
+        for (S[0] = P,i = 1; i < t; i++) {
+            S[i] = S[i - 1] + Q;
+        }
+        for (A = B = i = j = k = 0; k < 3 * t; k++,i = (i + 1) % t,j = (j + 1) % c) {// 3*t > 3*c
+            A = S[i] = (((S[i] + (A + B)) << (3 & (w - 1))) | ((S[i] + (A + B))>>>(w - (3 & (w - 1)))));
+            B = L[j] = (((L[j] + (A + B)) << ((A + B) & (w - 1))) | ((L[j] + (A + B))>>>(w - ((A + B) & (w - 1)))));
+        }
+    }
+    
 }
